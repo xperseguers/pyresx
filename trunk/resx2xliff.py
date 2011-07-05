7917164fedc5628f4bd11b9e9d02626fce7c138d
fedor.tyurin@gmail.com <fedor.tyurin@gmail.com@8d99776a-ff03-821b-665f-5434f8ec6aa5>
2011-07-05 20:24:49 +0000

    find_or_create_element supplementary function

--- trunk/resx2xliff.py
+++ trunk/resx2xliff.py
@@ -2,6 +2,32 @@ import os
 from xml import sax
 from xml.dom import minidom
 
+def find_or_create_element(doc, parent_element, tag_name, attrs=None):
+    child_element = None
+
+    for node in parent_element.childNodes:
+        if node.nodeType != minidom.Node.ELEMENT_NODE:
+            continue
+        element = node
+        if element.tagName == tag_name:
+            conform = True
+            if attrs:
+                for (key, value) in attrs.iteritems():
+                    if element.getAttribute(key) != value:
+                        conform = False
+                        break
+            if conform:
+                child_element = element
+                break
+    if child_element is None:
+        child_element = doc.createElement(tag_name)
+        if attrs:
+            for (key, value) in attrs.iteritems():
+                child_element.setAttribute(key, value)
+        parent_element.appendChild(child_element)
+    
+    return child_element
+
 class ResxConverter:
     class ContentHandler(sax.handler.ContentHandler):
         def __init__(self):
@@ -151,45 +177,16 @@ class ResxConverter:
         if xliff.tagName != 'xliff':
             raise Exception('Invalid root element')
 
-        file_element = None
-        for node in xliff.childNodes:
-            if node.nodeType != minidom.Node.ELEMENT_NODE:
-                continue
-            element = node
-            if element.tagName == 'file':
-                if element.getAttribute('original') == source_file:
-                    file_element = element
-                    break
-        if file_element is None:
-            file_element = doc.createElement('file')
-            file_element.setAttribute('original', source_file)
-            xliff.appendChild(file_element)
+        file_element = find_or_create_element(doc, xliff, 
+                                              'file', {'original': source_file})
 
-        body_element = None
-        for node in file_element.childNodes:
-            if node.nodeType != minidom.Node.ELEMENT_NODE:
-                continue
-            element = node
-            if element.tagName == 'body':
-                body_element = element
-                break
-        if body_element is None:
-            body_element = doc.createElement('body')
-            file_element.appendChild(body_element)
+        body_element = find_or_create_element(doc, file_element, 'body')
 
         for item in data:
-            tu_element = None
-            for node in body_element.childNodes:
-                if node.nodeType != minidom.Node.ELEMENT_NODE:
-                    continue
-                element = node
-                if element.tagName == 'trans-unit':
-                    tu_element = element
-                    break
-            if tu_element is None:
-                tu_element = doc.createElement('trans-unit')
-                tu_element.setAttribute('id', item['name'])
-                body_element.appendChild(tu_element)
+            tu_element = find_or_create_element(doc, body_element, 
+                                                'trans-unit', {'id': item['name']})
+            source_element = find_or_create_element(doc, tu_element, 'source')
+            target_element = find_or_create_element(doc, tu_element, 'target')
 
         with open(target_file_name, 'w') as f:
             f.write(doc.toxml())
