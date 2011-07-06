08a647267fcac9fd6d7617ee339d44eb21acf4d5
fedor.tyurin <fedor.tyurin@gmail.com>
2011-07-06 17:08:11 +0000

    Options parser

--- resx2xliff.py
+++ resx2xliff.py
@@ -3,6 +3,7 @@ from xml.parsers import expat
 from xml import sax
 from xml.dom import minidom
 from domext import PrettyPrint
+from optparse import OptionParser
 
 def find_or_create_element(doc, parent_element, tag_name, attrs=None):
     child_element = None
@@ -233,3 +234,20 @@ class ResxConverter:
     def run(self):
         self.scan_dir()
         self.update_translations()
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('-s', '--source', 
+                      help='root directory with source files (.resx)',
+                      default='.',
+                      metavar='SOURCE')
+    parser.add_option('-t', '--target',
+                      help='root directory for generated files (.xlf)', 
+                      default='.',
+                      metavar='TARGET')
+    (options, args) = parser.parse_args()
+    
+    default_lang_code = 'en'
+    converter = ResxConverter(options.source, 
+                              options.target, default_lang_code)
+    converter.run()
