5d7f71a0909ee7c1fea6fc2840536ceeb7666050
fedor.tyurin@gmail.com <fedor.tyurin@gmail.com@8d99776a-ff03-821b-665f-5434f8ec6aa5>
2011-07-06 17:39:02 +0000

    BaseContentHandler

--- trunk/utils.py
+++ trunk/utils.py
@@ -1,4 +1,5 @@
 from xml.dom import minidom
+from xml import sax
 
 def find_or_create_element(doc, parent_element, tag_name, attrs=None):
     child_element = None
@@ -30,3 +31,33 @@ def set_text(doc, element, text):
     for node in element.childNodes:
         element.removeChild(node)
     element.appendChild(doc.createTextNode(text))
+
+class BaseContentHandler(sax.handler.ContentHandler):
+    def startDocument(self):
+        self.elements = []
+        self.current_text_handler = None
+        self.current_end_handler = None
+            
+    def endDocument(self):
+        pass
+        
+    def startElement(self, name, attrs):
+        self.elements.append(name)
+        for handler in self.handlers:
+            if handler[0] == self.elements:
+                handler[1](attrs)
+                if len(handler) > 2:
+                    self.current_text_handler = handler[2]
+                if len(handler) > 3:
+                    self.current_end_handler = handler[3]
+
+    def endElement(self, name):
+        if not self.current_end_handler is None:
+            self.current_end_handler()
+        self.elements.pop()
+        self.current_text_handler = None
+        self.current_end_handler = None
+            
+    def characters(self, data):
+        if not self.current_text_handler is None:
+            self.current_text_handler(data)
